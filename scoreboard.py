6907023fa343d226b3a3e6af7095719aee28c171
Marcin Szpytma <ma.marcinsz@gmail.com>
2023-11-08 10:06:38 +0100

    Added option to log highscore in a text file

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('highscore_log.txt') as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -22,12 +23,11 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('highscore_log.txt', mode='w') as data:
+                data.write(f'{self.high_score}')
         self.score = 0
         self.update_scoreboard()
 
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
